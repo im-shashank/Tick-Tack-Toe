119823974a2e478c89347418206c43a22745205a
shashank.bhardwaj2003 <shashank.bhardwaj2003@gmail.com>
2024-09-28 00:40:42 +0530

    Added the working logic for checking player's winning move

--- scenes/scripts/SinglePlayer.gd
+++ scenes/scripts/SinglePlayer.gd
@@ -15,19 +15,6 @@ var winner = ""
 	["", "", ""]
 ]
 
-# A map containting all the adjacent positions possible for a speciific position
-@onready var buttonAdjacencyMap = {
-	"1": [[0, 1], [1, 0], [1, 1]],
-	"2": [[0, 0], [0, 2], [1, 1]],
-	"3": [[0, 1], [1, 2], [1, 1]],
-	"4": [[1, 1], [0, 2], [2, 2]],
-	"5": [[0, 0], [0, 1], [0, 2], [1, 0], [1, 2], [2, 0], [2, 1], [2, 2]],
-	"6": [[0, 0], [2, 0], [1, 1]],
-	"7": [[1, 0], [1, 1], [2, 1]],
-	"8": [[2, 0], [1, 1], [2, 2]],
-	"9": [[2, 1], [1, 1], [1, 2]]
-}
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -45,6 +32,7 @@ func _process(delta: float) -> void:
 
 func checkWinner():
 	#algorithm that will check who is the winner
+
 	#check row
 	for n in 3:
 		if grid[n][0] == grid[n][1] && grid[n][1] == grid[n][2] && grid[n][0] == grid[n][2]:
@@ -60,28 +48,65 @@ func checkWinner():
 		return grid[0][0]
 	elif grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0] && grid[0][2] == grid[2][0]:
 		return grid[0][2]
-	
 
-func checkForAdjacentX():
-	#this function will check for adjacent x to the current position of the x
-	var possibleAdjacentPositions: Array = buttonAdjacencyMap[currentButtonPressed]
-	for i in (possibleAdjacentPositions.size()):
-		var possiblePositionArray: Array = possibleAdjacentPositions[i]
-		var x_position: int = possiblePositionArray[0]
-		var y_position: int = possiblePositionArray[1]
-		print(grid[x_position][y_position])
-			
-	pass
+
+#this method will check for the next move that player requires to win the game
+func checkPlayerWinningMove():
+	var winningMoves: Array = []
+	#check if the winning move is in a row
+	var xRowCount: int = 0
+	for n in 3:
+		if (((grid[n][0] == grid[n][1]) && (grid[n][0] == playerChoice && grid[n][1] == playerChoice))
+		|| ((grid[n][1] == grid[n][2]) && (grid[n][1] == playerChoice && grid[n][2] == playerChoice))
+		|| ((grid[n][0] == grid[n][2]) && (grid[n][0] == playerChoice && grid[n][2] == playerChoice))):
+			for i in 3:
+				if grid[n][i] != playerChoice && grid[n][i] == "":
+					winningMoves.append([n, i])
+
+	#check if the winning move is in a column
+	for n in 3:
+		if (((grid[0][n] == grid[1][n]) && (grid[0][n] == playerChoice && grid[1][n] == playerChoice))
+		|| ((grid[1][n] == grid[2][n]) && (grid[1][n] == playerChoice && grid[2][n] == playerChoice))
+		|| ((grid[0][n] == grid[2][n]) && (grid[0][n] == playerChoice && grid[2][n] == playerChoice))):
+			for i in 3:
+				if grid[i][n] != playerChoice && grid[i][n] == "":
+					winningMoves.append([i, n])
+	
+	#TODO: fix this part of the logic
+	#check if the winning move is in a diagonal
+	if ((((grid[0][0] == grid[1][1]) && (grid[0][0] == playerChoice && grid[1][1] == playerChoice)))
+	|| ((grid[1][1] == grid[2][2]) && (grid[1][1] == playerChoice && grid[2][2] == playerChoice))
+	|| ((grid[0][0] == grid[2][2]) && (grid[0][0] == playerChoice && grid[2][2] == playerChoice))):
+		if grid[0][0] == "":
+			winningMoves.append([0, 0])
+		if grid[1][1] == "":
+			winningMoves.append([1, 1])
+		if grid[2][2] == "":
+			winningMoves.append([2, 2])
+	
+	if ((((grid[0][2] == grid[1][1]) && (grid[0][2] == playerChoice && grid[1][1] == playerChoice)))
+	|| ((grid[1][1] == grid[2][0]) && (grid[1][1] == playerChoice && grid[2][0] == playerChoice))
+	|| ((grid[0][2] == grid[2][0]) && (grid[0][2] == playerChoice && grid[2][0] == playerChoice))):
+		if grid[0][2] == "":
+			winningMoves.append([0, 2])
+		if grid[1][1] == "":
+			winningMoves.append([1, 1])
+		if grid[2][0] == "":
+			winningMoves.append([2, 0])
+
+	return winningMoves
 
 
 func _on_button_1_pressed() -> void:
 	currentButtonPressed = "1"
-	if grid[0][0] == "":
+	if currentTurn == "x" && grid[0][0] == "":
 		turn += 1
 		var x1 = x.get_child(0)
 		x1.visible = true
 		grid[0][0] = "X"
-		checkForAdjacentX()
+		currentTurn = "o"
+		var winningMove = checkPlayerWinningMove()
+		print(winningMove)
 	elif currentTurn == "o" && grid[0][0] == "":
 		turn += 1
 		var o1 = o.get_child(0)
@@ -93,13 +118,14 @@ func _on_button_1_pressed() -> void:
 
 func _on_button_2_pressed() -> void:
 	currentButtonPressed = "2"
-	checkForAdjacentX()
 	if currentTurn == "x" && grid[0][1] == "":
 		turn += 1
 		var x2 = x.get_child(1)
 		x2.visible = true
 		grid[0][1] = "X"
 		currentTurn = "o"
+		var winningMove = checkPlayerWinningMove()
+		print(winningMove)
 	elif currentTurn == "o" && grid[0][1] == "":
 		turn += 1
 		var o2 = o.get_child(1)
@@ -111,13 +137,14 @@ func _on_button_2_pressed() -> void:
 
 func _on_button_3_pressed() -> void:
 	currentButtonPressed = "3"
-	checkForAdjacentX()
 	if currentTurn == "x" && grid[0][2] == "":
 		turn += 1
 		var x3 = x.get_child(2)
 		x3.visible = true
 		grid[0][2] = "X"
 		currentTurn = "o"
+		var winningMove = checkPlayerWinningMove()
+		print(winningMove)
 	elif currentTurn == "o" && grid[0][2] == "":
 		turn += 1
 		var o3 = o.get_child(2)
@@ -129,13 +156,14 @@ func _on_button_3_pressed() -> void:
 
 func _on_button_4_pressed() -> void:
 	currentButtonPressed = "4"
-	checkForAdjacentX()
 	if currentTurn == "x" && grid[1][2] == "":
 		turn += 1
 		var x4 = x.get_child(3)
 		x4.visible = true
 		grid[1][2] = "X"
 		currentTurn = "o"
+		var winningMove = checkPlayerWinningMove()
+		print(winningMove)
 	elif currentTurn == "o" && grid[1][2] == "":
 		turn += 1
 		var o4 = o.get_child(3)
@@ -147,13 +175,14 @@ func _on_button_4_pressed() -> void:
 
 func _on_button_5_pressed() -> void:
 	currentButtonPressed = "5"
-	checkForAdjacentX()
 	if currentTurn == "x" && grid[1][1] == "":
 		turn += 1
 		var x5 = x.get_child(4)
 		x5.visible = true
 		grid[1][1] = "X"
 		currentTurn = "o"
+		var winningMove = checkPlayerWinningMove()
+		print(winningMove)
 	elif currentTurn == "o" && grid[1][1] == "":
 		turn += 1
 		var o5 = o.get_child(4)
@@ -165,13 +194,14 @@ func _on_button_5_pressed() -> void:
 
 func _on_button_6_pressed() -> void:
 	currentButtonPressed = "6"
-	checkForAdjacentX()
 	if currentTurn == "x" && grid[1][0] == "":
 		turn += 1
 		var x6 = x.get_child(5)
 		x6.visible = true
 		grid[1][0] = "X"
 		currentTurn = "o"
+		var winningMove = checkPlayerWinningMove()
+		print(winningMove)
 	elif currentTurn == "o" && grid[1][0] == "":
 		turn += 1
 		var o6 = o.get_child(5)
@@ -183,13 +213,14 @@ func _on_button_6_pressed() -> void:
 
 func _on_button_7_pressed() -> void:
 	currentButtonPressed = "7"
-	checkForAdjacentX()
 	if currentTurn == "x" && grid[2][0] == "":
 		turn += 1
 		var x7 = x.get_child(6)
 		x7.visible = true
 		grid[2][0] = "X"
 		currentTurn = "o"
+		var winningMove = checkPlayerWinningMove()
+		print(winningMove)
 	elif currentTurn == "o" && grid[2][0] == "":
 		turn += 1
 		var o7 = o.get_child(6)
@@ -201,13 +232,14 @@ func _on_button_7_pressed() -> void:
 
 func _on_button_8_pressed() -> void:
 	currentButtonPressed = "8"
-	checkForAdjacentX()
 	if currentTurn == "x" && grid[2][1] == "":
 		turn += 1
 		var x8 = x.get_child(7)
 		x8.visible = true
 		grid[2][1] = "X"
 		currentTurn = "o"
+		var winningMove = checkPlayerWinningMove()
+		print(winningMove)
 	elif currentTurn == "o" && grid[2][1] == "":
 		turn += 1
 		var o8 = o.get_child(7)
@@ -219,13 +251,14 @@ func _on_button_8_pressed() -> void:
 
 func _on_button_9_pressed() -> void:
 	currentButtonPressed = "9"
-	checkForAdjacentX()
 	if currentTurn == "x" && grid[2][2] == "":
 		turn += 1
 		var x9 = x.get_child(8)
 		x9.visible = true
 		grid[2][2] = "X"
 		currentTurn = "o"
+		var winningMove = checkPlayerWinningMove()
+		print(winningMove)
 	elif currentTurn == "o" && grid[2][2] == "":
 		turn += 1
 		var o9 = o.get_child(8)
